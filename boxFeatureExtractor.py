28a463820f814956917d22abf8731aaaf316e65e
Vojtech Kloda <vojtech.kloda@gmail.com>
2025-04-12 15:28:39 +0200

    removed deprecated files

--- boxFeatureExtractor.py
+++
@@ -1,8 +0,0 @@
-import jobManager as jm
-from detectionBoxes import DetectionBoxes
-
-filepaths, video_to_frame_indices_map, frame_idx_to_frame_path_map, frame_path_to_frame_idx_map = jm.get_images_metadata()
-
-jm.save_clip_box_features(filepaths, "boxFeatures.pickle")
-
-
--- jobExecutor.py
+++
@@ -1,4 +0,0 @@
-import jobManager as jm
-
-jm.process_jobs()
-jm.get_job_stats()
