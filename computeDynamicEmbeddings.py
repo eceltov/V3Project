00bc673f2d85f6a9914ebde6d5e7f918912dd280
Vojtech Kloda <vojtech.kloda@gmail.com>
2025-04-26 16:24:22 +0200

    added extra comments

--- computeDynamicEmbeddings.py
+++ computeDynamicEmbeddings.py
@@ -3,25 +3,17 @@ import datetime
 
 print(f"Job started at: {datetime.datetime.now()}")
 
-import lib.rectangles as rectangles
+import lib.dynamicAnalysisTool as dat
 
-# rects = [
-#   [0, 0, 50, 100],
-#   [0, 100, 50, 200],
-#   [500, 0, 550, 100],
-#   [500, 100, 550, 200],
-# ]
-# centroids, sorted_rects = rectangles.get_centroids_and_sorted_rects(rects, 2)
-# print(centroids)
-# print(sorted_rects)
+rects = [
+  # two rects atop each other
+  [0, 0, 50, 100],
+  [0, 100, 50, 200],
 
-# representing_rect = rectangles.get_representing_rect(centroids[0], sorted_rects[0], 5000)
-# print(representing_rect)
-
-rect = [
-  -10,
-  10,
-  90,
-  110,
+  # same two rects but shifted 500 pixels to the right
+  [500, 0, 550, 100],
+  [500, 100, 550, 200],
 ]
-print(rectangles.confine_to_area(100, 100, rect))
+
+representing_rects = dat.get_representing_rects(rects, 2, 1000)
+print(representing_rects)
--- lib/dynamicAnalysisTool.py
+++ lib/dynamicAnalysisTool.py
@@ -4,13 +4,30 @@ import lib.rankCalculations as rc
 import lib.boundaries as boundaries
 import lib.rectangles as rectangles
 
-def get_representing_rects(rects, kmeans_k, representing_rect_area):
+def get_representing_rects(rects: list[list], kmeans_k: int, representing_rect_area: int) -> list[list]:
+  """Takes a list of rectangles and clusters them using the kmeans algorithm.
+      Defines new rectangles centered around kmean clusters with a given area (all such rectangles have the same area).
+      These rectangles have a shape derived from the shape of the input rectangles linked to the matching centroid
+      (widths and heights are summed and divided by the same factor to match the @representing_rect_area)
+      In case the representing rectangles are outside of the frame, they are shifted.
+
+  Args:
+      rects (list[list]): A list of [x1, y1, x2, y2] lists representing input rectangles.
+      kmeans_k (int): How many clusters should there be.
+      representing_rect_area (int): The area (in pixels) of how big each representing rectangle should be.
+
+  Returns:
+      list[list]: Returns a list of [x1, y1, x2, y2] lists representing the rectangles centered around kmeans centroids.
+      This list is not sorted and can be ordered differently in identical calls.
+  """
+
+  # get centroids and linked rects
   centroids, sorted_rects = rectangles.get_centroids_and_sorted_rects(rects, kmeans_k)
   representing_rects = []
   for i in range(kmeans_k):
     # draw a rectangle around the centroid
     representing_rect = rectangles.get_representing_rect(centroids[i], sorted_rects[i], representing_rect_area)
-    # move the rectangle so that it is fully on the frame
+    # move the rectangle so that it is fully in the frame
     moved_representing_rect = rectangles.confine_to_area(pt.frame_width, pt.frame_height, representing_rect)
     representing_rects.append(moved_representing_rect)
   return representing_rects
