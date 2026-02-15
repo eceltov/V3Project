4b0f5cfa1a72d7bde9196b89c77cd1ccf0ab5d54
Kloda Vojtech Mgr. <klodav@gpulab.ms.mff.cuni.cz>
2026-02-15 18:28:58 +0100

    added method to get result rank indices

--- lib/rankCalculations.py
+++ lib/rankCalculations.py
@@ -13,3 +13,14 @@ def get_frame_rank(text, frame_idx, embeds, model, tokenizer):
     sorted_indices = torch.argsort(distances)[0].tolist()
     frame_rank = sorted_indices.index(frame_idx)
     return frame_rank
+
+def get_frame_rank_and_sorted_indices(text, frame_idx, embeds, model, tokenizer):
+  query = tokenizer(text).to(pt.device)
+
+  with torch.no_grad(), torch.amp.autocast(pt.device):
+    text_embeds = model.encode_text(query)
+
+    distances = 1 - (F.normalize(text_embeds) @ F.normalize(embeds).T)
+    sorted_indices = torch.argsort(distances)[0].tolist()
+    frame_rank = sorted_indices.index(frame_idx)
+    return frame_rank, sorted_indices
